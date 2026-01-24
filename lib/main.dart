8ae5eb870a9e6cf78e9538a191dd988b1eed891a
shunya-karan <karanyad9072@gmail.com>
2026-01-24 19:57:39 +0530

    Updated app theme and main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'teacher/teacher_login.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
+      title: "Attendance App",
       debugShowCheckedModeBanner: false,
       home: TeacherLogin(),
+      theme: ThemeData(
+          fontFamily: GoogleFonts.lato().fontFamily,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.teal,
+            primary: Colors.blue,
+            secondary: Colors.green,
+            error: Colors.red,
+        ),
+        appBarTheme: AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontSize: 20,
+            color: Colors.black
+          ),
+        ),
+        textTheme: TextTheme(
+          titleSmall: TextStyle(
+            fontWeight: FontWeight.bold,
+              fontSize: 16
+          ),
+          titleMedium: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20
+          ),
+          titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 35
+          )
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16
+          ),
+        ),
+        useMaterial3: true
+      ),
     );
   }
 }
